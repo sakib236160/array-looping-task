ae87ffaa3885d14d68977ce034accea23fe54b7c
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-10 11:34:49 -0800

    task 03 solved is 3 step

--- task03.js
+++ task03.js
@@ -7,9 +7,25 @@ Output:
     'TomTimTinTik'
  */
 
+// Step 1
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// console.log(numbers.join(''));
+
+
+// Step 2
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// var concatenate = '';
+// for(const num of numbers){
+//     concatenate = concatenate + num;
+// }
+// console.log(concatenate);
+
+
+//Step 3 
+
 var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
 let concatenate = '';
 for(const num of numbers){
-    concatenate = concatenate + num;
+    concatenate = num.concat('').concat(concatenate);
 }
 console.log(concatenate);
