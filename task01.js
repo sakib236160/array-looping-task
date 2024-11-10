ef66d2d4fc74715c87fa87d3e395d27230f38378
Md.Sakib Hossain <86706077+sakib236160@users.noreply.github.com>
2024-11-10 10:15:24 -0800

    task 01 solved in 5 deffirent way

--- task01.js
+++ task01.js
@@ -6,4 +6,51 @@ Input:
 Output:
         ['orange', 'yellow', 'green', 'blue', 'red']
 */
+// Step 01
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// colors.reverse();
+// console.log(colors);
 
+// Step 02
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const rev_color =[];
+// for(const color of colors){
+//     // console.log(color);
+//     rev_color.unshift(color);
+// }
+// console.log(rev_color);
+
+
+// Step 03
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const rev_color = [];
+// for(let i = 0; i<colors.length; i++){
+//     const color = colors[i];
+//     // console.log(color);
+//     rev_color.unshift(color);
+// }
+// console.log(rev_color);
+
+
+// Step 04
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const rev_color = [];
+// for(let i = colors.length - 1; i>= 0; i--){
+//     const color = colors[i];
+//     // console.log(color);
+//     rev_color.push(color);
+// }
+// console.log(rev_color);
+
+// Step 05
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+
+let i = 0;
+const rev_color =[];
+while(i<colors.length){
+    const color = colors[i]
+    // console.log(color);
+    rev_color.unshift(color)
+    i++;
+}
+console.log(rev_color);
